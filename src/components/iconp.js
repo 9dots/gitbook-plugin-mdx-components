f3946d0ab7d5d4f597fcf777e6591805bf39a628
Daniel <danleavitt0@gmail.com>
2016-11-14 10:52:35 -0800

    add margin top to icons

--- src/components/iconp.js
+++ src/components/iconp.js
@@ -32,7 +32,12 @@ const types = {
 function render ({props, children}) {
   const {icon, src, iconSize, type} = props
   return (
-    <Block ml={(icon || type) && '3.5em'} color={type ? types[type].color : 'inherit'} align={src ? 'start center' : 'start start'} {...props}>
+    <Block
+    	ml={(icon || type) && '3.5em'}
+    	color={type ? types[type].color : 'inherit'}
+    	align={src ? 'start center' : 'start start'}
+    	{...props}
+    	mt={src && '1em'}>
       {(icon || type) && <Icon mr='5px' lineHeight='1.1' name={type ? types[type].icon : icon}/>}
       {src && <Avatar mr='10px' mt='10px' size='50' src={src}/>}
       <Box align='start start' flex><Marked style={{display: 'flex'}}>{children}</Marked></Box>
